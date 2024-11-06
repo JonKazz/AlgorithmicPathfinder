b5c46bf0a7e5f7429519bcd6bed4a7e40049424f
JonKazz <112956618+JonKazz@users.noreply.github.com>
2024-11-06 12:37:21 -0600

    finished bfs algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use macroquad::prelude::*;
 use crate::search::BFSState;
+use macroquad::prelude::*;
 
 mod button;
 mod constants;
 mod draw;
 mod inputs;
-mod tile;
 mod search;
+mod tile;
 
 use button::Button;
 use constants::{buttons, grid};
@@ -15,21 +15,40 @@ use constants::{buttons, grid};
 async fn main() {
     let buttons = [
         Button::new(buttons::right_buttons_x(), grid::y_pos(), "CLEAR", true),
-        Button::new(buttons::right_buttons_x(), grid::y_pos() + buttons::button_distance_y(), "PLACE WALL", true),
-        Button::new(buttons::right_buttons_x(), grid::y_pos() + buttons::button_distance_y() * 2.0, "START FLAG", true),
-        Button::new(buttons::right_buttons_x(), grid::y_pos() + buttons::button_distance_y() * 3.0, "END FLAG", true),
-        Button::new(buttons::left_buttons_x(), grid::y_pos() + buttons::button_distance_y(), "SEARCH", false),
+        Button::new(
+            buttons::right_buttons_x(),
+            grid::y_pos() + buttons::button_distance_y(),
+            "PLACE WALL",
+            true,
+        ),
+        Button::new(
+            buttons::right_buttons_x(),
+            grid::y_pos() + buttons::button_distance_y() * 2.0,
+            "START FLAG",
+            true,
+        ),
+        Button::new(
+            buttons::right_buttons_x(),
+            grid::y_pos() + buttons::button_distance_y() * 3.0,
+            "END FLAG",
+            true,
+        ),
+        Button::new(
+            buttons::left_buttons_x(),
+            grid::y_pos() + buttons::button_distance_y(),
+            "SEARCH",
+            false,
+        ),
     ];
 
     let mut mode = WHITE;
     let grid = [[tile::Tile::new(0.0, 0.0, 50.0, WHITE); grid::NUM_TILES]; grid::NUM_TILES];
-    
+
     let mut visual_handler = draw::VisualHandler::new(30, grid, buttons);
     let mut input_handler = inputs::InputHandler::new();
     let mut window_size = (screen_width(), screen_height());
     let mut start_flag = (grid::NUM_TILES + 1, grid::NUM_TILES + 1);
     let mut end_flag = (grid::NUM_TILES + 1, grid::NUM_TILES + 1);
-
 
     let mut bfs_state: Option<BFSState> = None;
 
@@ -38,17 +57,18 @@ async fn main() {
 
         visual_handler.zoom_grid();
         visual_handler.draw_grid(mode);
-        input_handler.handle_inputs(&mut visual_handler, &mut mode, &mut start_flag, &mut end_flag);
-        let zl = visual_handler.zoom_level;
-        print!("{zl}\n");
-
+        input_handler.handle_inputs(
+            &mut visual_handler,
+            &mut mode,
+            &mut start_flag,
+            &mut end_flag,
+        );
 
         if mode == RED {
             if bfs_state.is_none() {
                 bfs_state = Some(BFSState::new(start_flag, end_flag));
             }
 
-            
             if let Some(state) = &mut bfs_state {
                 let finished = state.step(&mut visual_handler).await;
                 if finished {
--- src/search.rs
+++ src/search.rs
@@ -1,97 +1,99 @@
-use crate::draw;
 use crate::constants::grid;
+use crate::draw;
 use macroquad::prelude::*;
 use std::collections::VecDeque;
 
 pub struct BFSState {
     q: VecDeque<(usize, usize)>,
     end_flag: (usize, usize),
-    start_flag: (usize, usize),
-    travel_grid: [[u16; grid::NUM_TILES]; grid::NUM_TILES],
-    level: u16,
+    travel_grid: [[i16; grid::NUM_TILES]; grid::NUM_TILES],
     found_path: bool,
+    color_scale: f32,
 }
 
 impl BFSState {
     pub fn new(start_flag: (usize, usize), end_flag: (usize, usize)) -> Self {
         let mut q = VecDeque::new();
-        let travel_grid = [[(grid::NUM_TILES*grid::NUM_TILES) as u16; grid::NUM_TILES]; grid::NUM_TILES];
-        let level = 0;
+        let mut travel_grid = [[-1; grid::NUM_TILES]; grid::NUM_TILES];
+        travel_grid[start_flag.0][start_flag.1] = 0;
         let found_path = false;
+        let color_scale = 0.2;
         q.push_back(start_flag);
-        BFSState { q, end_flag, start_flag, travel_grid, level, found_path }
+        BFSState {
+            q,
+            end_flag,
+            travel_grid,
+            found_path,
+            color_scale,
+        }
     }
 
     pub async fn step(&mut self, vh: &mut draw::VisualHandler) -> bool {
-        if let Some(tile) = self.q.pop_front() {
-            if tile == self.end_flag || self.found_path {
-                self.found_path = true;
-                if self.create_path(vh) {
-                    return true;
-                }
-            }
+        if self.found_path {
+            return self.create_path(vh);
+        } else if let Some(tile) = self.q.pop_front() {
+            let (r, c) = (tile.0 as i16, tile.1 as i16);
+            let directions = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
+            let lower = (grid::NUM_TILES as i16 - vh.zoom_level as i16) / 2;
+            let upper = lower + vh.zoom_level as i16;
 
-            let r: i16 = tile.0 as i16;
-            let c: i16 = tile.1 as i16;
-            let directions: [(i16, i16); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+            for &(dr, dc) in &directions {
+                let (nr, nc) = (r + dr, c + dc);
 
-            for (dr, dc) in directions.iter() {
-                let nr = r + *dr;
-                let nc = c + *dc;
+                if (lower..upper).contains(&nr) && (lower..upper).contains(&nc) {
+                    if self.end_flag == (nr as usize, nc as usize) {
+                        self.found_path = true;
+                        self.travel_grid[nr as usize][nc as usize] =
+                            self.travel_grid[r as usize][c as usize] + 1;
+                        break;
+                    }
 
-                let lower = (grid::NUM_TILES as i16 - vh.zoom_level as i16) / 2;
-                let upper = lower + vh.zoom_level as i16;
-
-                if lower <= nr && nr < upper as i16 && lower <= nc && nc < upper as i16 {
-                    if vh.grid[nr as usize][nc as usize].color != PINK && vh.grid[nr as usize][nc as usize].color != BLACK {
+                    let grid_color = vh.grid[nr as usize][nc as usize].color;
+                    if self.travel_grid[nr as usize][nc as usize] < 0 && grid_color != BLACK {
                         self.q.push_back((nr as usize, nc as usize));
-                        vh.grid[nr as usize][nc as usize].color = PINK;
-                        self.travel_grid[tile.0][tile.1] = self.level;
-                        self.level += 1;
+                        vh.grid[nr as usize][nc as usize].color = Color::new(
+                            self.color_scale * 2.0,
+                            0.8 - self.color_scale,
+                            0.3 - self.color_scale * 1.5,
+                            1.0,
+                        );
+
+                        self.travel_grid[nr as usize][nc as usize] =
+                            self.travel_grid[r as usize][c as usize] + 1;
                     }
                 }
             }
-        }
-        else {
-            return true;      
-        }
-
-    false
-    }
-
-
-    pub fn create_path(&mut self, vh: &mut draw::VisualHandler) -> bool {
-        if self.end_flag == self.start_flag {
+            self.color_scale += 0.01 * (1.0 / vh.zoom_level as f32);
+        } else {
             return true;
         }
-    
-        let r: i16 = self.end_flag.0 as i16;
-        let c: i16 = self.end_flag.1 as i16;
-        let mut next_tile = None;
-        let mut min_distance = self.travel_grid[self.end_flag.0][self.end_flag.1];
+
+        false
+    }
+
+    pub fn create_path(&mut self, vh: &mut draw::VisualHandler) -> bool {
+        if self.travel_grid[self.end_flag.0][self.end_flag.1] == 1 {
+            return true;
+        }
+
+        let (r, c) = (self.end_flag.0 as i16, self.end_flag.1 as i16);
+        let current_distance = self.travel_grid[self.end_flag.0][self.end_flag.1];
         let directions: [(i16, i16); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-    
-        for (dr, dc) in directions.iter() {
-            let nr = r + *dr;
-            let nc = c + *dc;
-            let lower = (grid::NUM_TILES as i16 - vh.zoom_level as i16) / 2;
-            let upper = lower + vh.zoom_level as i16;
-    
-            if lower <= nr && nr < upper && lower <= nc && nc < upper {
-                let neighbor = (nr as usize, nc as usize);
-                if self.travel_grid[neighbor.0][neighbor.1] < min_distance {
-                    min_distance = self.travel_grid[neighbor.0][neighbor.1];
-                    next_tile = Some(neighbor);
+
+        for (dr, dc) in &directions {
+            let (nr, nc) = (r + dr, c + dc);
+
+            if 0 <= nr && nr < grid::NUM_TILES as i16 && 0 <= nc && nc < grid::NUM_TILES as i16 {
+                let neighbor_distance = self.travel_grid[nr as usize][nc as usize];
+                if neighbor_distance == current_distance - 1 {
+                    self.end_flag = (nr as usize, nc as usize);
+                    vh.grid[self.end_flag.0][self.end_flag.1].color = YELLOW;
+                    break;
                 }
             }
         }
-    
-        if let Some(next) = next_tile {
-            self.end_flag = next;
-            vh.grid[self.end_flag.0][self.end_flag.1].color = YELLOW;
-        }
-    
+
         false
     }
 }
